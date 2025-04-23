d927dd7eda11f4db92c36c4364a74aae7a6331d7
RegimentSnare2004 <becca.godfrey@bruins.belmont.edu>
2025-04-23 00:07:46 -0500

    Finished problem 4

--- src/Problem4.java
+++ src/Problem4.java
@@ -2,38 +2,61 @@ public class Problem4 {
 
     public static void main(String[] args){
 
-        int[][] input = {
-                {0,1,0},
-                {0,0,1},
-                {1,0,0}
+        int[][] input1 = {
+                {0,1,0,0},
+                {1,0,1,0},
+                {0,1,0,1},
+                {0,0,1,0}
         };
 
-        if(isGraphDirected(input)){
+        int[][] input2 = {
+                {0,1,0,0},
+                {0,0,1,0},
+                {0,0,0,1},
+                {0,1,0,0}
+        };
+
+        System.out.println("Input 1: ");
+        if(isGraphDirected(input1)){
             System.out.println("The matrix represents a directed graph.");
         }
         else{
-            System.out.println("The matrix does not represent a directed graph.");
+            System.out.println("The matrix represents an undirected graph.");
+        }
+
+        System.out.println("Input 2: ");
+        if(isGraphDirected(input2)){
+            System.out.println("The matrix represents a directed graph.");
+        }
+        else{
+            System.out.println("The matrix represents an undirected graph.");
         }
 
     }
 
     public static boolean isGraphDirected(int[][] input){
-        boolean result = true;
+        boolean result = false;
         int n = input.length;
 
         for(int[] row: input){
             if(row.length != n){
-                return false;
+                System.out.println("invalid input");
             }
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(input[i][j] != 0 && input[i][j] != 1){
-                    return false;
+                    System.out.println("invalid input");
+
+                }
+                if(input[i][j] != input[j][i]){
+                    return true;
                 }
             }
         }
+
+
 
 
         return result;
